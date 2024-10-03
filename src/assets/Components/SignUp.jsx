028d4bb0db632ce9ab83348c1330c3b74ffa88ce
Mumtaz97gani <143265576+Mumtaz97gani@users.noreply.github.com>
2024-10-03 17:00:05 +0530

    Update SignUp.jsx

--- src/assets/Components/SignUp.jsx
+++ src/assets/Components/SignUp.jsx
@@ -15,45 +15,45 @@ function SignUp() {
     });
   }
   return (
-    <div className="d-flex mt-5 justify-content-center align-items-center">
-      <Form onSubmit={handleSubmit}>
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-              type="email"
-              onChange={(e) => {
-                setInputData({ ...inputData, email: e.target.value });
-              }}
-              placeholder="Enter email"
-            />
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              onChange={(e) => {
-                setInputData({ ...inputData, password: e.target.value });
-              }}
-              placeholder="Password"
-            />
-          </Form.Group>
-        </Row>
-
-        <Form.Group className="mb-3" id="formGridCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-
-        <Button variant="primary" type="submit">
-          SignUp
-        </Button>
-        <Link to="/">
-          <Button variant="primary" type="submit" className="ms-2">
-            back to logIn
-          </Button>
-        </Link>
-      </Form>
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <h1 className="text-center mb-6 signup-H">Create an Account</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <input
+                type="email"
+                onChange={(e) => {
+                  setInputData({ ...inputData, email: e.target.value });
+                }}
+                className="form-control"
+                placeholder="Email"
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <input
+                type="password"
+                onChange={(e) => {
+                  setInputData({ ...inputData, password: e.target.value });
+                }}
+                className="form-control"
+                placeholder="Password"
+                required
+              />
+            </div>
+            <button type="submit" className="btn btn-success w-100">
+              Sign Up
+            </button>
+          </form>
+          <p className="mt-3 text-center signup-H">
+            Already have an account?{" "}
+            <Link to="/" className="my-link">
+              login
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
